18602f2c431aca0744282207f5468f15a3afee40
Dan Bright <dan@uplandsdynamic.com>
2022-12-22 21:04:24 +0000

    Update URLs

--- app/UFOzone/UFOzone/urls.py
+++ app/UFOzone/UFOzone/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("ie.urls"), kwargs={}, name="ie"),
-    path("ie/", include("ie.urls"), kwargs={}, name="ie"),
     path("data/", include("dataview.urls"), kwargs={}, name="dataview"),
     path("admin/", admin.site.urls),
 ] 
